7dac5348fbe52be8998a93d0a6da729920c9a9b4
cew88 <66454866+cew88@users.noreply.github.com>
2021-06-18 16:26:22 -0700

    Update getTrainingData.py

--- getTrainingData.py
+++ getTrainingData.py
@@ -1,34 +1,40 @@
-import os, sys, random
+import os, random
+from optparse import OptionParser
 import pandas as pd
 from decimal import Decimal
 
+def CommandLineParser():
+    parser = OptionParser()
+    parser.add_option("-f", "--file", dest="inputFile", default="", help="Input cluster file of clustered CDR3 sequences")
+    parser.add_option("-l", "--labels", dest="labelFile", default="", help="Labeled CSV with sample IDs and donor type")
+    parser.add_option("-s", "--size", dest="clusterSize", default="50", help="Cluster size")
+    parser.add_option("-p", "--purity", dest="clusterPurity", default="0.80", help="Cluster purity")
+    parser.add_option("-o", "--outputDIR", dest="outDIR", default="TrainingData", help="Output directory for training and validation sets. If not given, a directory named 'TrainingData' will be created to hold the training and validation data files.")
+    return parser.parse_args()
+
+(opt,_) = CommandLineParser()
+inputFilename = opt.inputFile
+inputLabelname = opt.labelFile
+inputSize = int(opt.clusterSize)
+inputPurity = float(opt.clusterPurity)
+outDIR = opt.outDIR
+
+if (len(inputFilename)==0 or len(inputLabelname)==0):
+    print("Must provide an input cluster file and a labeled CSV with sample ID and donor type.")
+    exit()
 
 # clusterPurityDict stores value (+1 malignant, +0 benign) for all seq. in a cluster
 clusterPurityDict = {}
 # clusterSizeDict stores total number of sequences in a cluster
 clusterSizeDict = {}
 
-try: 
-    inputFilename = sys.argv[1]
-    inputLabels = pd.read_csv(sys.argv[2])
-except:
-    print ("Please provide input files.")
-    exit()
-
-try:
-    inputSize = int(sys.argv[3])
-    inputPurity = float(sys.argv[4])
-except:
-    # Default: use cluster size <= 50 and cluster purity >= 80% if no arguments are provided
-    inputSize = 50
-    inputPurity = 0.8
-
+# Collects information about size and purity for each cluster
+inputFile = open(inputFilename)
+inputLabels = pd.read_csv(inputLabelname)
 
 allBenign = inputLabels.index[inputLabels['Benign/Malignant'] == "Benign"].tolist()
 allMalignant = inputLabels.index[inputLabels['Benign/Malignant'] == "Malignant"].tolist()
 
-# Collects information about size and purity for each cluster
-inputFile = open(inputFilename)
 for fLine in inputFile.readlines():
     lineArr = list(fLine.split('\t'))
     sampleName = lineArr[-1][:-1]
@@ -83,52 +89,53 @@ for fLine in inputFile.readlines():
                     cancerDict['cl' + str(len(seq))].append(seq)
 
             # Classify clusters as healthy
-            elif (purity <= float(Decimal('1')-Decimal(str(inputPurity)))
-                ):
+            elif (purity <= float(Decimal('1')-Decimal(str(inputPurity)))):
                 if ('nl' + str(len(seq)) in nonCancerDict):
                     nonCancerDict['nl' + str(len(seq))].append(seq)
 
 
-# Shuffle and distribute sequences for training and validations via 80/20 split
-MalignantTrain = []
-MalignantEval = []
-BenignTrain = []
-BenignEval = []
+def getTrainingandValidation(cancerDict, nonCancerDict):
+    # Shuffle and distribute sequences for training and validations via 80/20 split
+    MalignantTrain = []
+    MalignantEval = []
+    BenignTrain = []
+    BenignEval = []
 
-for i in range(12, 18):
-    random.Random(4).shuffle(cancerDict["cl" + str(i)])
-    random.Random(4).shuffle(nonCancerDict["nl" + str(i)])
+    for i in range(12, 18):
+        random.Random(4).shuffle(cancerDict["cl" + str(i)])
+        random.Random(4).shuffle(nonCancerDict["nl" + str(i)])
 
-    c = cancerDict['cl' + str(i)]
-    n = nonCancerDict["nl" + str(i)]
+        c = cancerDict['cl' + str(i)]
+        n = nonCancerDict["nl" + str(i)]
 
-    train_cancer, val_cancer = c[: int(len(c) * .8)],  c[int(len(c) * .8):] 
-    train_noncancer, val_noncancer = n[: int(len(n) * .8)],  n[int(len(n) * .8):] 
+        train_cancer, val_cancer = c[: int(len(c) * .8)],  c[int(len(c) * .8):] 
+        train_noncancer, val_noncancer = n[: int(len(n) * .8)],  n[int(len(n) * .8):] 
 
-    MalignantTrain += train_cancer
-    MalignantEval += val_cancer
-    BenignTrain += train_noncancer
-    BenignEval += val_noncancer
+        MalignantTrain += train_cancer
+        MalignantEval += val_cancer
+        BenignTrain += train_noncancer
+        BenignEval += val_noncancer
 
-# Create directory with training data files
-# newDir = "TrainingData"
-# os.mkdir(newDir)
-# with open (newDir + "/MalignantTrain.txt", "w") as f:
-#     for c in MalignantTrain:
-#         f.write(c + "\n")
-# f.close() 
+    # Create directory with training data files
+    os.mkdir(outDIR)
+    with open (outDIR + "/MalignantTrain.txt", "w") as f:
+        for c in MalignantTrain:
+            f.write(c + "\n")
+    f.close() 
 
-# with open (newDir + "/MalignantEval.txt", "w") as f:
-#     for c in MalignantEval:
-#         f.write(c + "\n")
-# f.close() 
+    with open (outDIR + "/MalignantEval.txt", "w") as f:
+        for c in MalignantEval:
+            f.write(c + "\n")
+    f.close() 
 
-# with open (newDir + "/BenignTrain.txt", "w") as f:
-#     for n in BenignTrain:
-#         f.write(n + "\n")
-# f.close()
+    with open (outDIR + "/BenignTrain.txt", "w") as f:
+        for n in BenignTrain:
+            f.write(n + "\n")
+    f.close()
 
-# with open (newDir + "/BenignEval.txt", "w") as f:
-#     for n in BenignEval:
-#         f.write(n + "\n")
-# f.close()
+    with open (outDIR + "/BenignEval.txt", "w") as f:
+        for n in BenignEval:
+            f.write(n + "\n")
+    f.close()
+
+getTrainingandValidation(cancerDict, nonCancerDict)
